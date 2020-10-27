5ca71095e5c78616dcc63f125017d2ebd2bf4329
Alexander <greenwald80@gmail.com>
2020-10-27 14:45:00 +0200

    added composition for high order components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -88,9 +88,9 @@ export default class App extends Component {
           <PlanetDetails itemId={5} />
           <StarshipDetails itemId={8} />
 
-          <PersonList>{({ name }) => <span>{name}</span>}</PersonList>
-          <PlanetList>{({ name }) => <span>{name}</span>}</PlanetList>
-          <StarshipList>{({ name }) => <span>{name}</span>}</StarshipList>
+          <PersonList></PersonList>
+          <PlanetList></PlanetList>
+          <StarshipList></StarshipList>
         </div>
       </ErrorBoundry>
     );
